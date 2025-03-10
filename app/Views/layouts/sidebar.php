51987fb578b8b2eee7db01da94949ad0928e8946
Alex <aldirifaldi821@gmail.com>
2025-03-10 09:59:24 +0700

    First Commit

---
+++ app/Views/layouts/sidebar.php
@@ -0,0 +1,31 @@
+<li class="menu-header">Dashboard</li>
+<li class="">
+    <a href="<?= base_url('dashboard') ?>" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+</li>
+<li class="menu-header">Data Master</li>
+<li class="nav-item dropdown">
+    <a href="#" class="nav-link" data-toggle="dropdown"><i class="fas fa-columns"></i>
+        <span>Barang</span></a>
+    <!-- <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+                                <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+                                <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+                            </ul> -->
+</li>
+<li class=""><a class="nav-link" href="#"><i class="far fa-square"></i>
+        <span>Pelanggan</span></a></li>
+<li class=""><a class="nav-link" href="#"><i class="fas fa-th"></i>
+        <span>Supplier</span></a>
+    <!-- <ul class="dropdown-menu">
+        <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
+        <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
+        <li><a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a></li>
+    </ul> -->
+</li>
+<li class="menu-header">Aktivitas</li>
+<li class=""><a class="nav-link" href="#"><i class="far fa-square"></i>
+        <span>Pengiriman</span></a></li>
+<li class=""><a class="nav-link" href="#"><i class="far fa-square"></i>
+        <span>Pesanan</span></a></li>
+<li class=""><a class="nav-link" href="#"><i class="far fa-square"></i>
+        <span>Keuangan</span></a></li>
